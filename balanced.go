fbf4f9f57848474333a2e59da7dde1b5500a2b4d
Bruce Ediger <bediger8@gmail.com>
2025-10-07 04:18:52 -0600

    fix panic on last char of string an opening parentheses

--- balanced.go
+++ balanced.go
@@ -23,7 +23,7 @@ func main() {
 
 	mismatch := false
 	for _, r := range runes {
-		if m, ok := matches[r]; ok {
+		if m, ok := matches[r]; len(stack) > 0 && ok {
 			top := stack[len(stack)-1]
 			if m != top {
 				fmt.Printf("Got %c, found %c on stack\n", r, top)
